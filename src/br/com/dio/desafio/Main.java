f5ad74b2804c65f30cf732ff0b9d3266b2d5df12
Lucas Daniel <ludaniel.sd@gmail.com>
2024-06-28 23:55:03 -0300

    Trabalhando a Abstração e Encapsulamento do Projeto

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso = new Curso();
+		curso.setTitulo("Curso de Java");
+		curso.setDescricao("Curso de POO em Java");
+		curso.setCargahoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso de Javascript");
+		curso2.setDescricao("Curso modificando o DOM javascript");
+		curso2.setCargahoraria(3);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria de C#");
+		mentoria.setDescricao("Desenvolvendo aplicação em ASP.NET");
+		mentoria.setData(LocalDate.now());;
+		
+		System.out.println(String.format("%s , %s , %d", curso.getTitulo(),curso.getDescricao(),curso.getCargahoraria()));
+		System.out.println(String.format("%s , %s , %d", curso2.getTitulo(),curso2.getDescricao(),curso2.getCargahoraria()));
+		System.out.println(String.format("%s , %s , %s", mentoria.getTitulo(),mentoria.getDescricao(),mentoria.getData()));
+		
+	}
+
+}
